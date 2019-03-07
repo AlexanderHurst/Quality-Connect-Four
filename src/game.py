5ac74de7231b12f553765c4ca0e91c526c4a7ba8
Alexander Hurst <alexander@ahmail.ca>
2019-03-06 23:34:42 -0500

    working game loop with board visualization and input need to implement win checking and error handling

--- src/game.py
+++ src/game.py
@@ -1,18 +1,49 @@
 class Board:
+    def __init__(self, dim):
+        self.dim = dim
+        self.board = [[0 for x in range(self.dim)] for i in range(self.dim)]
 
-    def __init__(self):
-        self.board = [[0 for x in range(8)] for i in range(8)]
         self.playerTwo = False
+        self.user_prompt = ""
+        self._set_user_prompt()
+
+        self.game_over = False
+
+        self.location_labels = "| "
+        self.location_labels += ''.join("{0} | ".format(i)
+                                        for i in range(1, self.dim + 1))
+
+    def move(self, location):
+        for row in self.board:
+            if row[location - 1] == 0:
+                row[location - 1] = int(self.playerTwo) + 1
+                break
+
+        self.playerTwo = not self.playerTwo
+        self._set_user_prompt()
+
+    def _set_user_prompt(self):
+        self.user_prompt = "player " + \
+            str(int(self.playerTwo) + 1) + " select a location: "
 
     def __str__(self):
-        return(str([row + "\n" for row in self.board]))
+        top_bot_border = "|---" + "+---" * (self.dim - 1) + "|"
+        temp = top_bot_border + "\n"
+        for row in reversed(self.board):
+            temp += "| "
+            for num in row:
+                temp += str(num) + " | "
+            temp += "\n"
+        temp += top_bot_border
+        return temp
 
 
-game_board = Board()
+game_board = Board(7)
 
-print(game_board)
-print([x for x in range(1, 9)])
+while (game_board.game_over == False):
+    print(game_board)
+    print(game_board.location_labels)
 
-usr_prompt = "player " + \
-    str(int(game_board.playerTwo) + 1) + " select a location:"
-location = input(usr_prompt)
+    game_board.move(int(input(game_board.user_prompt)))
+
+    print("\n\n\n")
